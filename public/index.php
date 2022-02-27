2d300921a9d9463ea58350078444d33b0f7f8f08
Tony Hermann <alejoh.developer@gmail.com>
2022-02-27 18:53:24 -0300

    .

--- public/index.php
+++ public/index.php
@@ -32,7 +32,8 @@ $router->get('/blog/actualizar', [BlogController::class, 'actualizar']);
 $router->post('/blog/actualizar', [BlogController::class, 'actualizar']);
 $router->post('/blog/eliminaradminpropiedades', [BlogController::class, 'eliminar']);
 
-$router->get('/', [PaginasController::class, 'index']);
+// $router->get('/', [PaginasController::class, 'index']);
+$router->get('/', [PaginasController::class, 'nosotros']);
 $router->get('/nosotros', [PaginasController::class, 'nosotros']);
 $router->get('/anuncios', [PaginasController::class, 'propiedades']);
 $router->get('/anuncio', [PaginasController::class, 'propiedad']);
