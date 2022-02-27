e954fee3cbb9420ca0bcb0ad7806b01828ba176c
Tony Hermann <alejoh.developer@gmail.com>
2022-02-27 13:56:17 -0300

    .

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function connectDb() {
-    $db = new mysqli("us-cdbr-east-05.cleardb.net", 'b07b5fc4ca8d25', '94648d17', 'heroku_edcd461d431c03e', '3306');
+    $db = new mysqli($hostname = "us-cdbr-east-05.cleardb.net", $username = 'b07b5fc4ca8d25', $password = '94648d17', $database = 'heroku_edcd461d431c03e', $port = '3306');
     if (!$db) {
         echo "Error, no se pudo conectar";
         echo $db;
