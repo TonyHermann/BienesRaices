05807ed6614169c75f8183983e37ea5325e61f55
Tony Hermann <alejoh.developer@gmail.com>
2022-02-27 13:02:27 -0300

    db set

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function connectDb() {
-    $db = new mysqli('127.0.0.1', 'root', '', 'bienes_raices', '3310');
+    $db = new mysqli('us-cdbr-east-05.cleardb.net', 'b07b5fc4ca8d25', '94648d17', 'bienes_raices', '3306');
     if (!$db) {
         echo "Error, no se pudo conectar";
         echo $db;
