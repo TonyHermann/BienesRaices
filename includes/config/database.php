4c48617e5dcd76f2ff3db29a64c2a7f3c59a5baf
Tony Hermann <alejoh.developer@gmail.com>
2022-02-27 13:39:23 -0300

    .

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function connectDb() {
-    $db = new mysqli("us-cdbr-east-05.cleardb.net", 'b07b5fc4ca8d25', '94648d17', 'bienes_raices', '3306');
+    $db = new mysqli("us-cdbr-east-05.cleardb.net", 'b07b5fc4ca8d25', '94648d17', 'bienes_raices', '3310');
     if (!$db) {
         echo "Error, no se pudo conectar";
         echo $db;
